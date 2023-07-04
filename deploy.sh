3f36af6b2cc9a9f8bc627ed39a52e341a40ccf73
luke0han <lukeohanlon@outlook.com>
2023-07-04 22:35:47 +0100

    error fix

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 sudo apt update && sudo apt install nodejs npm
 # Install pm2 which is a production process manager for Node.js with a built-in load balancer. sudo npm install -g pm2
+sudo npm install -g pm2
 # stop any instance of our application running currently
 pm2 stop example_app
 # change directory into folder where application is downloaded
