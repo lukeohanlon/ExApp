53f083d90f2e3dbf07b1c325a4bb374a3d890b8a
luke0han <lukeohanlon@outlook.com>
2023-07-06 22:32:10 +0100

    config

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/env bash
-sudo apt update && sudo apt install nodejs npm
-# Install pm2 which is a production process manager for Node.js with a built-in load balancer. sudo npm install -g pm2
-sudo npm install -g pm2
-# stop any instance of our application running currently
-pm2 stop example_app
-# change directory into folder where application is downloaded
 
+sudo apt update && sudo apt install nodejs npm
+
+sudo npm install -g pm2
+
+pm2 stop example_app
+
+cd ExApp
 
 npm install
 
